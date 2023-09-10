3256f248c06630bf8e0ac25a6b85c40ae814d182
Nathan Lima <nathanls.dev@gmail.com>
2023-09-09 22:44:47 -0300

    refactor: decoupling insertion of the changed elements into the DOM

--- lampada/app.js
+++ lampada/app.js
@@ -1,23 +1,30 @@
 const body = document.body;
 const lampada = document.querySelector("img");
 
+const ligado = {
+  imagemLampada: `https://img.freepik.com/fotos-premium/lampada-de-luz-amarela-acesa-em-branco_488220-82.jpg`,
+  nomeClasse: "ligado",
+};
+
+const desligado = {
+  imagemLampada: `https://imagens-revista-pro.vivadecora.com.br/uploads/2018/02/lampada-led-esquenta-lampada-incandescente-40w.jpg`,
+  nomeClasse: "desligado",
+};
+
 const controleInterruptor = ({ target }) => {
   const idElementoClicado = target.id;
 
-  if (idElementoClicado === "ligar") ligarLuz();
-  if (idElementoClicado === "desligar") desligarLuz();
+  if (idElementoClicado === "ligar") alterarStatusLampada(ligado);
+  if (idElementoClicado === "desligar") alterarStatusLampada(desligado);
 };
 
-const ligarLuz = () => {
-  const luzLigada = `https://img.freepik.com/fotos-premium/lampada-de-luz-amarela-acesa-em-branco_488220-82.jpg`;
-  body.setAttribute("class", "ligado");
-  lampada.src = luzLigada;
-};
+const alterarStatusLampada = (obj) => {
+  const { imagemLampada, nomeClasse } = obj;
 
-const desligarLuz = () => {
-  const luzDesLigada = `https://imagens-revista-pro.vivadecora.com.br/uploads/2018/02/lampada-led-esquenta-lampada-incandescente-40w.jpg`;
-  body.setAttribute("class", "desligado");
-  lampada.src = luzDesLigada;
+  lampada.src = imagemLampada;
+  body.setAttribute("class", nomeClasse);
 };
 
 body.addEventListener("click", controleInterruptor);
+
+//decoupling insertion of the changed elements into the DOM
