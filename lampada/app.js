b963ac4d6af28ef7de5ee329d6a0277c949dfbcc
Nathan Lima <nathanls.dev@gmail.com>
2023-09-09 21:54:07 -0300

    refactor: light-controlling exercise

--- lampada/app.js
+++ lampada/app.js
@@ -1,21 +1,23 @@
 const body = document.body;
 const lampada = document.querySelector("img");
-const ligar = document.querySelector("#ligar");
-const desligar = document.querySelector("#desligar");
+
+const controleInterruptor = ({ target }) => {
+  const idElementoClicado = target.id;
+
+  if (idElementoClicado === "ligar") ligarLuz();
+  if (idElementoClicado === "desligar") desligarLuz();
+};
 
 const ligarLuz = () => {
   const luzLigada = `https://img.freepik.com/fotos-premium/lampada-de-luz-amarela-acesa-em-branco_488220-82.jpg`;
-
   body.setAttribute("class", "ligado");
-  lampada.setAttribute("src", luzLigada);
+  lampada.src = luzLigada;
 };
 
 const desligarLuz = () => {
   const luzDesLigada = `https://imagens-revista-pro.vivadecora.com.br/uploads/2018/02/lampada-led-esquenta-lampada-incandescente-40w.jpg`;
-
   body.setAttribute("class", "desligado");
-  lampada.setAttribute("src", luzDesLigada);
+  lampada.src = luzDesLigada;
 };
 
-ligar.addEventListener("click", ligarLuz);
-desligar.addEventListener("click", desligarLuz);
+body.addEventListener("click", controleInterruptor);
